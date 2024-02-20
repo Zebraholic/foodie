1e1ca2b3b77f069e5ad1da1fb106735a938e2ebd
zebraholic <rctavares@rogers.com>
2024-02-19 20:31:10 -0400

    Update styling

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -44,7 +44,7 @@ const {bookmarkCollection} = data;
                 <h1>About Us</h1>
                 <p>Welcome to our website. We are a team of passionate food enthusiasts who love to explore different cuisines and share our experiences with others.</p>
                 <p>On this platform, you will find a variety of recipes, restaurant reviews, and cooking tips to help you on your culinary journey.</p>
-                <p>Feel free to browse through our content and get inspired to try new dishes or visit exciting food destinations.</p>
+                <p>Feeling free to browse through our content and get inspired to try new dishes or visit exciting food destinations.</p>
             </div>
             <Bookmarks bookmarks={bookmarkCollection.items} headline="My Bookmarks"/> 
         </>
